ce2caaaf524f87f65cf4bfef5a2b195c071dbed5
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-05-05 16:06:30 +0600

    wrote the best function ever 'hello world'

---
+++ 01.intro/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
+
+/**
+ * Step 1 - Editing
+ * Step 2 - Compiling
+ * Step 3 - Linking
+ * Step 4 - Executing
+ */
